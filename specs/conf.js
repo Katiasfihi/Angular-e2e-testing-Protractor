a7400625ba4d9721d954bdf22e3dae12f6f1fb9b
Katiasfihi <katia.sfihi@gmail.com>
2021-06-23 12:14:22 +0100

    java set up

--- specs/conf.js
+++ specs/conf.js
@@ -4,6 +4,8 @@ exports.config = {
   seleniumServerJar: 'D:/Eclipse progs/jars/selenium-server-standalone-3.11.0.jar',
   specs: ['D:ProtractorJSFilesLocation	est.js'],
   capabilities: {
-	browserName: 'chrome'
+	browserName: 'chrome',
+  seleniumPort: 4444,
+  seleniumArgs: [-help]
   }
-}
+} 
